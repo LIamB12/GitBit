b98f7b230344a358a0133b68a8af9bd95eb1c127
Liam Beenken <liam.beenken@gmail.com>
2024-08-10 12:54:47 -0400

    group commits by repo

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ if __name__ == "__main__":
     }
     event_data = requests.get("https://api.github.com/users/LIamB12/events", headers=headers)
     event_data = event_data.json()
-    commits = []
+    repo_changes = {}
 
     for event in event_data:
         if event.get("type") == "PushEvent":
+            repo = event.get('repo').get('name')
+            commits = event.get('payload').get('commits')
             commit_messages = []
-            for commit in event.get('payload').get('commits'):
-                commit_messages.append(commit.get('message'))
-            commit_data = {
-                "repo": event.get('repo').get('name'),
-                "commits": commit_messages
-            }
-            commits.append(commit_data)
+            for commit in commits:
+                commit_messages.append(commit.get("message"))
+            if repo in repo_changes:
+                repo_changes[repo] += commit_messages
+            else:
+                repo_changes[repo] = commit_messages
 
-    for commit in commits:
-        print("____________________________________________________________")
-        print("REPO:", commit.get('repo'))
-        for message in commit.get("commits"): 
-            print(message)
-        print("____________________________________________________________")
+    for repo in repo_changes:
+        print(repo + ":")
+        for message in repo_changes.get(repo): 
+            print("-", message)
+        print("")
