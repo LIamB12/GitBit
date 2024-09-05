084e8452c0233871cf171aaa988a5c4f9361ccf5
LIamB12 <liam.beenken@gmail.com>
2024-09-04 20:45:38 -0400

    fix edge case when event occasionally stored as a string

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 import requests
 import os
 from dotenv import load_dotenv
@@ -31,6 +32,8 @@ def main():
 
 
     for event in event_data:
+        if isinstance(event, str):
+            continue
         if event.get("type") != "PushEvent":
             continue
 
